fe58052b32be4a7da71c6ff48683358ef342c8ba
HwangHeeWon <hheewon99@gmail.com>
2023-07-03 21:27:40 +0900

    2023-07-02

--- myreact/src/Page/Home/Home.jsx
+++ myreact/src/Page/Home/Home.jsx
@@ -14,48 +14,93 @@ const Home = () => {
     grid-template-rows: 8% 92%;
   `
   const HeaderContainer = styled.div`
-    width: 50%;
+    width: 100%;
     margin: 0 auto;
-    display: grid;
+    display: flex;
     align-items: center;
-    grid-template-columns: 5% 25% 5%;
-    grid-column-gap: 32.5%;
+    justify-content: space-around;
+    grid-template-columns: 5% 40% 5%;
+    grid-column-gap: 17.5%;
     text-align: center;
     span{
       cursor: pointer;
     }
+    @media (min-width: 1536px) {
+      width: 50%;
+      display: grid;
+      grid-template-columns: 10% 30% 10%;
+      grid-column-gap: 25%;
+    }
   `
   const CalendarContainer = styled.div`
-
+    display: grid;
+    min-height: 500px;
+    grid-template-columns: repeat(7, 1fr);
+    grid-template-rows: 30px auto;
+    margin: 10px;
+    margin-top: unset;
+    div:nth-child(-n+7){
+      border-top: 1px solid #000;
+    }
+    div:nth-child(7n-6){
+      border-left: 1px solid #000;
+      p{color: #F00;}
+      p.PrevDay{color: #Faa;}
+    }
+    div:nth-child(7n){
+      p{color: #00F;}
+      p.NextDay{color: #aaf;}
+    }
   `
+
   const [DATE, setDATE] = useState(dayjs())
   const [calendar, setCalendar] = useState()
-
+    console.log(calendar)
   useEffect(()=>{
-    DateCalender()
+    DateCalendar()
   }, [])
 
   const ChangeDate = (e) => {
     setDATE(dayjs(DATE).subtract(e, 'month'))
   }
   useEffect(()=>{
-    DateCalender()
+    DateCalendar()
   },[DATE])
+  const KeyEvent = (e) => {
+    console.log(e)
+  };
 
-  const DateCalender = () => {
+  const DateCalendar = () => {  //이번달 한달 달력 그리기
     let date = []
     for(let i = dayjs(DATE).startOf('month').format('d'); i > 0; i--){
-      console.log(i,dayjs(DATE).startOf('month').format('d'))
-      date.unshift(<p>{dayjs(DATE).startOf('month').subtract(i, 'day').format('D')}</p>)
+      date.unshift(
+        <p 
+          id={dayjs(DATE).startOf('month').subtract(i, 'day').format('YYYY-MM-D')} 
+          className='Day PrevDay'>
+          {dayjs(DATE).startOf('month').subtract(i, 'day').format('D')}
+        </p>
+      )
     }
     for(let i = 1; i <= dayjs(DATE).endOf('month').format('D'); i++){
-      date.push(<p>{i}</p>)
+      date.push(
+        <p 
+          id={dayjs(i).format('YYYY-MM-D')} 
+          className='Day'>
+          {i}
+        </p>
+      )
     }
     for(let i = 1; i < (7-dayjs(DATE).endOf('month').format('d')); i++){
-      // console.log(i*-1)
-      date.push(<p>{dayjs(DATE).endOf('month').subtract(i*-1, 'day').format('D')}</p>)
+      date.push(
+        <p 
+          id={dayjs(DATE).endOf('month').subtract(i*-1, 'day').format('YYYY-MM-D')} 
+          className='Day NextDay'>
+          {dayjs(DATE).endOf('month').subtract(i*-1, 'day').format('D')}
+        </p>
+      )
     }
     setCalendar(date)
+
   }
   return (
     <MainContainer>
@@ -65,7 +110,19 @@ const Home = () => {
         <span onClick={()=>ChangeDate(-1)}>{dayjs(DATE).subtract(-1, 'month').format('M월')}</span>
       </HeaderContainer>
       <CalendarContainer>
-        {calendar}
+        <div className='Calendar Week'><span className='text-[#f00]'>일</span></div>
+        <div className='Calendar Week'><span>월</span></div>
+        <div className='Calendar Week'><span>화</span></div>
+        <div className='Calendar Week'><span>수</span></div>
+        <div className='Calendar Week'><span>목</span></div>
+        <div className='Calendar Week'><span>금</span></div>
+        <div className='Calendar Week'><span className='text-[#00f]'>토</span></div>
+        {
+          !!calendar&&
+          calendar.map((e,i) => {
+            return (<div key={i} className='Calendar' >{e}</div>)
+          })
+        }
       </CalendarContainer>
     </MainContainer>
   )
