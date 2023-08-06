58af171a98b7525bbdd7cedcd46a1a314f0f1549
HwangHeeWon <hheewon99@gmail.com>
2023-08-06 17:49:37 +0900

    2023-08-06

--- myreact/src/Page/Home/Calendar.jsx
+++ myreact/src/Page/Home/Calendar.jsx
@@ -5,59 +5,50 @@ import axios from 'axios'
 
 const Calendar = ({setPopup, DATE}) => {
     
-  const [Today, setToday] = useState(dayjs().format('YYYY-MM-DD'))  //오늘 날짜
+  const Today = dayjs().format('YYYY-MM-DD')  //오늘 날짜
   const [calendar, setCalendar] = useState()  //일 가져오기
   const [solDay, setSolDay] = useState({Year: dayjs().format('YYYY'), Month: dayjs().format('MM')});
-  const [getSpcdeInfo, setGetSpcdeInfo] = useState([]);
-  const numOfRows = [1,2,3,4]
-  const URL = "/SpcdeInfo/B090041/openapi/service/SpcdeInfoService/";
+  const [spcdeInfoData, setSpcdeInfoData] = useState([]);
+  const URL = "http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService";
   const Param = [
-    "getHoliDeInfo", 
-    "getRestDeInfo", 
-    "getAnniversaryInfo",	
-    "get24DivisionsInfo",	
-    "getSundryDayInfo"
+    "getHoliDeInfo",      // 1     getHoliDeInfo	    국경일 정보조회
+    "getRestDeInfo",      // 2		getRestDeInfo	    공휴일 정보조회
+    "getAnniversaryInfo",	// 3		getAnniversaryInfo	기념일 정보조회
+    "getSundryDayInfo",   // 4		getSundryDayInfo	잡절 정보조회
+    "get24DivisionsInfo"	// 5		get24DivisionsInfo	24절기 정보조회
   ]
-
-  useEffect(()=>{
+  const getSpcdeInfo = () => {
+    let value = []
     Param.map((e,i)=>{
-      if(!numOfRows.includes(i)){
-        axios.get(URL + e, {
-          params: {
-            ServiceKey: process.env.REACT_APP_SpcdeInfoService_Key,
-            solYear: solDay.Year,
-            solMonth: solDay.Month
-          }
-        })
-        .then((res)=>{
-          console.log(res);
-        })
-        .catch((err)=>{
-          console.error(err)
-        })
-      }else{
-        axios.get(URL + e, {
-          params: {
-            ServiceKey: process.env.REACT_APP_SpcdeInfoService_Key,
-            solYear: solDay.Year,
-            solMonth: solDay.Month,
-            numOfRows: 50 ,
-            pageNo: 1
-          }
-        })
-        .then((res)=>{
-          console.log(res)
-        })
-        .catch((err)=>{
-          console.error(err)
-        })
-      }
+      // axios.post(`${URL}/${e}`,{
+      //   ServiceKey:process.env.REACT_APP_SpcdeInfoService_Key,
+      //   solYear:solDay.Year, 
+      //   solMonth:solDay.Month,
+      //   _type:'json'
+      // })
+      axios({
+        url:`${URL}/${e}?solYear=${solDay.Year}&solMonth=${solDay.Month}&ServiceKey=${process.env.REACT_APP_SpcdeInfoService_Key}&_type=json`
+      })
+      .then((res)=>{
+        value[i] = res.data.response.body.items.item;
+      })
+      .catch((err)=>{
+        console.error(err)
+      })
     })
-  },[])
-
+    setSpcdeInfoData([...spcdeInfoData, value])
+  }
   useEffect(()=>{
+    console.log(spcdeInfoData)
+  },[spcdeInfoData])
+  useEffect(()=>{
+    setSolDay({Year: DATE.$y, Month: dayjs(DATE).format('MM')})
     DateCalendar()
   },[DATE])
+  useEffect(()=>{
+    getSpcdeInfo()
+  },[solDay])
+
   const dailyPop = (e) => { //날짜 클릭시 팝업 띄우는 함수
     setPopup({state: true, year: dayjs(e).format('YYYY'), month: dayjs(e).format('MM'), day: dayjs(e).format('D'), date: dayjs(e).format('dd')})
   }
